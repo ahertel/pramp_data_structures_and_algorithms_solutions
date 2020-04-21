6c508ceeda867d844c52398c510244e268fda580
ahertel <herteladrian@gmail.com>
2020-04-21 00:50:44 -0400

    Update ToeplitzMatrix.py
    T: O(N), S: O(1)

--- ToeplitzMatrix.py
+++ ToeplitzMatrix.py
@@ -1,45 +1,71 @@
-def isToeplitz(arr):
-  # we don't have to evaluate the bottom left and top right corners
-  # compare [0][0], [1,1]
-  i = x = 0
-  is_toeplitz = True
-  #start one up from bottom left row because bottom left corner doesn't need to be evaluated
-  #we'll check em all for simplicity sake
-  # not to check out of index because non empty
+'''
+A Toeplitz matrix is a matrix where every left-to-right-descending diagonal has the same element. Given a non-empty matrix arr, write a function that returns true if and only if it is a Toeplitz matrix. The matrix can be any dimensions, not necessarily square.
 
-  cols = len(arr[0])
-  rows = len(arr)
-  
-  for c in range(cols-1, -1, -1): # from 2 to 0
-    r 
-    c = 0
-    last_val = arr[i][x]
-    r += 1
-    c += 1
+For example,
 
-    while r < rows and c < cols: # 3 and 4
-      print('comparing ' + str(last_val) + ' to ' + str(arr[r][c]))
-      is_toeplitz = False if last_val != arr[i][x] else True
-      last_val = arr[r][c]
-      # move one down and across 
-      r += 1
-      c += 1
-      print('eol: i = ' + str(r) + ", x = " + str(c))
-    if c == 0:
-      c += 1
-  '''
-  if len(arr) > 0:
-    for i in range(len(arr)): # 0 to 2
-      for x in range(len(arr[0])): # 0 to 3
-        if arr[i][x]
-    return
-  '''
-  return is_toeplitz
-
-print (isToeplitz([[1,2,3,4],
+[[1,2,3,4],
  [5,1,2,3],
- [6,5,1,2]]))
+ [6,5,1,2]]
+is a Toeplitz matrix, so we should return true, while
 
-print( isToeplitz([[1,2,3,4],
+[[1,2,3,4],
  [5,1,9,3],
- [6,5,1,2]]))
+ [6,5,1,2]]
+isn’t a Toeplitz matrix, so we should return false.
+
+Constraints:
+
+[time limit] 5000ms
+[input] array.array.integer arr
+0 ≤ arr.length ≤ 20
+0 ≤ arr[i].length ≤ 20
+0 ≤ arr[i][j] ≤ 20
+[output] boolean
+'''
+def isToeplitz(arr):
+	cols = len(arr[0])
+	rows = len(arr)
+	
+	# intialize starting position to bottom left corner
+	r_s = rows - 1
+	c_s = 0
+	start_positions = rows + cols - 1 # the number of possible starting position
+	
+	# navigates along the left edge and then the top edge for starting positions from which check left-to-right descending diagonals
+	for i in range(start_positions):
+		r = r_s
+		c = c_s
+		last_val = arr[r][c]
+			
+		# increment down and across
+		r += 1
+		c += 1
+
+		# navigate down and across from starting position until bounds are reached
+		while r < rows and c < cols:
+			if last_val != arr[r][c]:
+				return False
+			last_val = arr[r][c]
+			# move one down and across 
+			r += 1
+			c += 1
+		
+		r_s -= 1 # move up the start row along the left edge
+		# if arrived at top of left edge, move start position one column to the right
+		if r_s == -1:
+			r_s = 0
+			c_s +=1 # move over start column
+
+	return True
+
+# should return True
+print (isToeplitz([
+	[1,2,3,4],
+	[5,1,2,3],
+	[6,5,1,2]]))
+
+# should return False
+print( isToeplitz([
+	[1,2,3,4],
+	[5,1,9,3],
+	[6,5,1,2]]))
